8536641f1543bf8d359ea1c41b908046f4c6411d
Yakunin Anton <thisistobik@gmail.com>
2022-11-20 18:21:00 +0300

    Switch int to long types

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {1, 2, 8, 11});
+        SalesManager salesManager = new SalesManager(new long[] {1, 2, 8, 11});
         System.out.println(SalesManager.max());
     }
     }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,17 @@
 public class SalesManager {
-    protected static int[] sales;
+    protected static long[] sales;
 
-    public SalesManager(int[] sales) {
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
 
     public static int max() {
-        int max = -1;
-        for (int sale : sales) {
+        long max = -1;
+        for (long sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
-        return max;
+        return (int) max;
     }
 }
